15e71c72e8984549b855e97d8e2451cf25fd78ae
RomainCabaret <123275998+RomainCabaret@users.noreply.github.com>
2023-02-05 15:22:31 +0100

    Initial project

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace passwordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            const int NB_PASSWORD = 4;
+
+            const int MINCHOICE = 1;
+            const int MAXCHOICE = 4;
+
+
+            int passwordLenght = outils.AskPositiveNumberNotNull("Longeur du mot de passe : ");
+            int userChoice = outils.AskNumberBetween("\nVous voulez un mot de passe avec :\n" +
+                "1 - Uniquement des caractères en minuscule \n" +
+                "2 - Des caractères minuscules et majuscules \n" +
+                "3 - Des caractères et des chiffres \n" +
+                "4 - Caractère, chiffres et caractères spéciaux \n" +
+                "Votre choix : ", MINCHOICE, MAXCHOICE) ;
+
+            string miniscules = "abcdefghijklmnopkrstuvwxyz";
+            string majuscules = miniscules.ToUpper();
+            string number = "0123456789";
+            string specialChar = "#&+-*";
+
+            string alphabet;
+
+            Random rnd = new Random();
+
+            switch (userChoice)
+            {
+                case 1:
+                    alphabet = miniscules;
+                    break;
+                case 2:
+                    alphabet = miniscules + majuscules;
+                    break;
+                case 3:
+                    alphabet = miniscules + number + number;
+                    break;
+                default:
+                    alphabet = miniscules + number + number + specialChar;
+                    break;
+            }
+
+            int alphabetlenght = alphabet.Length;
+
+            for(int i = 0; i < NB_PASSWORD; i++)
+            {
+                string password = "";
+
+                for (int j = 0; j < passwordLenght; j++)
+                {
+                    int index = rnd.Next(0, alphabetlenght);
+
+                    password += alphabet[index];
+                }
+                Console.WriteLine($"\n - {password}");
+            }
+        }
+    }
+}
---
+++ outils.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+
+namespace passwordGenerator
+{
+    static class outils
+    {
+        public static int AskPositiveNumberNotNull(string question)
+        {
+            return AskNumberBetween(question, 1, int.MaxValue, "ERREUR : Le nombre doit être positif et non nul");
+        }
+        public static int AskNumberBetween(string question, int min, int max, string customError = null)
+        {
+            int userAnswer = AskNumber(question);
+
+            while (userAnswer < min || userAnswer > max)
+            {
+                if (customError == null)
+                {
+                    Console.WriteLine($"ERREUR le nombre doit etre compris entre {min} et {max}\n");
+
+                }
+                else
+                {
+                    Console.WriteLine(customError);
+                }
+                userAnswer = AskNumber(question);
+            }
+
+            return userAnswer;
+        }
+
+        static int AskNumber(string question)
+        {
+
+            int result = 0;
+
+            while (true)
+            {
+                Console.Write(question);
+                string userAnswer = Console.ReadLine();
+                try
+                {
+                    result = int.Parse(userAnswer);
+                    return result;
+                }
+                catch
+                {
+                    Console.WriteLine("ERREUR : vous devez rentrer un nombre");
+                }
+            }
+        }
+    }
+}
